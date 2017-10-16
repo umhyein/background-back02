1e64ca271ec34fc0453a4716e8aca2699fda28cc
haley.um <haley.um@kakaocorp.com>
2017-10-16 10:28:24 +0900

    Initial commit

---
+++ src/main/java/common/controller/UserController.java
@@ -0,0 +1,17 @@
+package common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by umhyein on 2017. 10. 12..
+ */
+@RestController
+public class UserController {
+
+    @GetMapping("test")
+    public String test(){
+        return "test";
+    }
+
+}
